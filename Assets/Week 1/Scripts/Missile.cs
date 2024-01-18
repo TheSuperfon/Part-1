b3204b01c3ee0f66ff155bbb529d88b54746e038
auburn <astefanadon@gmail.com>
2024-01-18 14:23:12 -0500

    Week 2 task 1 complete collisions are happening

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
-    float speed = 5f;
+    Vector2 direction = new Vector2(10,0);
+    Rigidbody2D rigidbody;
+    //float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(1 * speed * Time.deltaTime, 0, 0);
+        //transform.Translate(1 * speed * Time.deltaTime, 0, 0);
     }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("I Hit It");
+        Destroy(gameObject);
+    }
+
+
 }
